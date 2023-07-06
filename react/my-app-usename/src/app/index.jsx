3e997e408e3b1d157aa2dd3e9477992c291465e8
YuraAstapuk <uraostapuk243@gmail.com>
2023-07-06 17:38:04 +0200

    corrected version of the first task

--- react/my-app-usename/src/app/index.jsx
+++ react/my-app-usename/src/app/index.jsx
@@ -1,4 +1,3 @@
-
 // Установите новое приложение с помощью npx create-react-app my-app-name
 // Создайте input, 2 кнопки add и clear, пустой div
 // Приложение должно работать следующим образом
@@ -8,28 +7,66 @@
 //     При нажатии на clear надписи в div должны очищаться Для выполнения задания используйте хук useState
 
 import React, { useState } from "react";
+//первая версия задачи , неправильно сделана ! при записи в инпут сразу идет запись в div , и при нажатии на add слова множаться 
+// function App() {
+//   const [words, setWords] = useState(" ");
+
+//   const handleAdd = () => {
+//     setWords((prevWords) => (prevWords ? `${prevWords}-${words}` : words));
+//   };
+
+//   const handleClear = () => {
+//     setWords(" ");
+//   };
+
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         value={words}
+//         onChange={(e) => setWords(e.target.value)}
+//       />
+//       <button onClick={handleAdd}>Add</button>
+//       <button onClick={handleClear}>Clear</button>
+//       <div>{words}</div>
+//     </div>
+//   );
+// }
 
 function App() {
-  const [words, setWords] = useState(" ");
+  const [inputValue, setInputValue] = useState("");
+  const [words, setWords] = useState([]);
 
-  const handleAdd = () => {
-    setWords((prevWords) => (prevWords ? `${prevWords}-${words}` : words));
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);//обновление значения ввода
   };
 
-  const handleClear = () => {
-    setWords("");
+  const handleAddClick = () => {//эта функция добавляет слово в список вводимых слов и обнуляет значение ввода
+    if (inputValue.trim() !== "") {
+      setWords((prevWords) => [...prevWords, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const handleClearClick = () => {
+    setWords([]);//очищает значение поля ввода
   };
 
   return (
     <div>
-      <input
-        type="text"
-        value={words}
-        onChange={(e) => setWords(e.target.value)}
-      />
-      <button onClick={handleAdd}>Add</button>
-      <button onClick={handleClear}>Clear</button>
-      <div>{words}</div>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleAddClick}>Add</button>
+      <button onClick={handleClearClick}>Clear</button>
+      <div>
+        {words.map(
+          (
+            word,
+            index //с помощью метода map преобразовываем каждый элемент массива в JSX компонент , создавая каждому слову массива span
+          ) => (
+            <span key={index}>{index === 0 ? word : "-" + word}</span> //Проверяем индекс элемента и добавляем - все кроме 0 элемента массива
+          )
+        )}
+      </div>
     </div>
   );
 }
